659e3d6689c7b8105fa67bba3dc707267bf97390
nsdiv <nsdiv@hotmail.com>
2015-12-16 15:25:45 -0800

    https://github.com/spring-projects/spring-boot/issues/4794

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -4,14 +4,24 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class Application {
     
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Application.class)
+                .initializers(new ApplicationContextInitializer<GenericApplicationContext>() {
+                    @Override
+                    public void initialize(GenericApplicationContext applicationContext) {
+                        applicationContext.setAllowCircularReferences(false);
+                    }
+                }).run(args);
+
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         
         String[] beanNames = ctx.getBeanDefinitionNames();
